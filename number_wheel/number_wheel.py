0ff3da2a3b37da9911b80e3564b846a8a47608e7
heldeo <heldeo3@hotmail.com>
2019-11-08 12:13:29 -0600

    refactor of number_wheel

--- number_wheel/number_wheel.py
+++ number_wheel/number_wheel.py
@@ -1,5 +1,4 @@
-
-
+#Get user input
 def get_user() -> list:
     returnList = []
     lock = str(input("List char:"))
@@ -7,27 +6,30 @@ def get_user() -> list:
     while(lock != "q"):
         returnList.append(lock)
         raw_val = str(input("List vals for the input character seperated by a space: "))
-    
-        
         raw_val = list(raw_val.split(" "))
         vals.append(raw_val)
         lock = str(input("List char: "))
-
     return returnList,vals
+
+#two list => dictionary
 def user_preprocess(returned_char,returned_vals):
     return {char:vals for char,vals in zip(returned_char,returned_vals)}
+
+#traverse dictionary entry's list, and recursively call all char definitions
+#else print the number
 def traverse(hash_map,list_vals):
     for i in range(len(list_vals)):
         if list_vals[i].isalpha():
             traverse(hash_map,hash_map[list_vals[i]])
         else:
             print(list_vals[i],end=" " )
+
+#start traversal
 def algo(hash_map):
     for keys,vals in hash_map.items():
         traverse(hash_map,vals)
+#main:        
 returned_char,returned_vals = get_user()
 final_input_dict = user_preprocess(returned_char,returned_vals)
 print(final_input_dict,"\n Generates: ",end = " " )
 algo(final_input_dict)
-
-
